a38d5084793206830d4d97ce743184c0f440b895
Jonatan Cardoso <jonatancardosoc@gmail.com>
2023-11-02 23:15:33 -0300

    "Sidebar" component configuration

---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src='https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Background Perfil'
+            />
+
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/JoonatanCardoso.png"/>
+                
+                <strong>Jonatan Cardoso</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href='#'>
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
